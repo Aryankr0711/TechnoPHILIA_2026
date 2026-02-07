0499cb2f345d2dddfa5819a821006d658014b381
Aryankr0711 <aryankr0711@example.com>
2026-02-07 18:46:56 +0530

    Update ISA logo to ISA_Logo.jpeg

--- src/components/Association.js
+++ src/components/Association.js
@@ -7,7 +7,7 @@ const Association = () => {
       <div className="association-content">
         <h3 className="association-title">In Association With</h3>
         <div className="association-logos">
-          <img src="/NEW_ISA_Logo.png" alt="ISA Logo" className="association-logo" />
+          <img src="/ISA_Logo.jpeg" alt="ISA Logo" className="association-logo" />
           <img src="/NEW_IEEE_Logo.png" alt="IEEE Logo" className="association-logo" />
           <img src="/supista_logo.png" alt="Supista Logo" className="association-logo" />
           <img src="/Spark_Logo.jpg" alt="Spark Logo" className="association-logo" />
